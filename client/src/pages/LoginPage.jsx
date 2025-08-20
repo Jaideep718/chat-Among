6a4daea70b31c710f2d1cb3aa876836521914d78
Jaideep718 <reddy.jaideep18@gmail.com>
2025-08-20 18:16:12 +0530

    updated loginpage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -93,10 +93,6 @@ const LoginPage = () => {
         >
           {currentState === "Sign Up" ? "Create Account" : "Login Now"}
         </button>
-        <div className="flex items-center gap-2 text-sm text-gray-500">
-          <input type="checkbox" />
-          <p>Agree to the terms of use & privacy policy.</p>
-        </div>
         <div className="flex flex-col gap-2">
           {currentState === "Sign Up" ? (
             <p className="text-sm text-gray-600">
